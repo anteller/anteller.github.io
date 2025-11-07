a9615c9ff182312bd60629d329039b2974a7753a
antel <amiya2022@yahoo.co.jp>
2025-11-07 20:09:00 +0900

    Updated src/constants.js with new constants and settings.

--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,12 @@ export const SETTINGS_KEY = "quiz_settings_v1";
 export const GENRE_ORDER_KEY = "quiz_genre_order_v1";
 export const DATA_VERSION = 2;
 
+export const APP_MODES = {
+  SINGLE: "single",
+  MULTIPLE: "multiple",
+  FLASHCARDS: "flashcards"
+};
+
 export const DEFAULT_SETTINGS = {
   progressMode:"manual",
   autoDelaySeconds:1.0,
@@ -10,7 +16,8 @@ export const DEFAULT_SETTINGS = {
   themeMode:"auto",
   undoDurationSeconds:7,
   shuffleChoices:false,
-  priorityIncreaseMultiplier:1.4
+  priorityIncreaseMultiplier:1.4,
+  appMode: APP_MODES.SINGLE
 };
 
 export const MIN_CHOICES=2;
