d27702460f4701a45ff5a69ffbaa75414c3aee9d
antel <amiya2022@yahoo.co.jp>
2025-11-07 19:34:50 +0900

    2
    Added mobile OS detection to apply specific styles.

--- src/main.js
+++ src/main.js
@@ -6,18 +6,25 @@ import { safeLoadQuizzes, loadSettings, loadGenreOrder } from "./storage.js";
 import { applyTheme, showScreen } from "./utils.js";
 import { rebuildGenreButtons } from "./manage.js";
 import { bindEvents } from "./events.js";
-import { setupGenreMenu } from "./genreMenu.js"; // 追加: ジャンルカードの三点メニュー設定
+import { setupGenreMenu } from "./genreMenu.js"; // 三点メニュー
+// ***** モバイルOS判定 *****
+function isMobileOS(){
+  const ua = navigator.userAgent.toLowerCase();
+  return /android/.test(ua) || /iphone|ipad|ipod/.test(ua);
+}
 
 // ***** 初期化 *****
 function init(){
+  if(isMobileOS()){
+    document.body.classList.add("is-mobile-os");
+  }
   state.quizzes = safeLoadQuizzes();
   loadSettings();
   loadGenreOrder();
   rebuildGenreButtons();
-  setupGenreMenu(); // 追加: ジャンルカードへメニューボタン注入＋監視
+  setupGenreMenu();
   applyTheme();
   showScreen("genreSelect");
-  // ここで他の初期フォーム（追加画面など）の初期化が必要なら呼び出し
   bindEvents();
 }
 
